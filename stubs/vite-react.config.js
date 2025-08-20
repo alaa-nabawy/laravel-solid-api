8a82e3642da6afde00f5b7729e5e83191da86973
alaa-nabawy <nabawyalaa97@yahoo.com>
2025-08-20 23:35:41 +0300

    fix vite config

--- stubs/vite-react.config.js
+++ stubs/vite-react.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
         host: "0.0.0.0",
         port: 5173,
         hmr: {
-            host: "localhost",
+            host: "0.0.0.0",
         },
     },
     test: {
